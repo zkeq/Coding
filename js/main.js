a1e9cd74ca20c946b7ee765cb92417b3d31cfd42
zkeq <62864752+zkeq@users.noreply.github.com>
2021-09-20 11:59:35 +0800

    music

--- js/main.js
+++ js/main.js
@@ -865,3 +865,37 @@ document.addEventListener('DOMContentLoaded', function () {
   refreshFn()
   unRefreshFn()
 })
+
+
+const ap = new APlayer({
+  container: document.getElementById('player'),
+  mini: false,
+  autoplay: false,
+  theme: '#FADFA3',
+  loop: 'all',
+  order: 'random',
+  preload: 'auto',
+  volume: 0.7,
+  mutex: true,
+  listFolded: false,
+  listMaxHeight: 90,
+  lrcType: 3,
+  audio: [
+      {
+          name: 'name1',
+          artist: 'artist1',
+          url: 'url1.mp3',
+          cover: 'cover1.jpg',
+          lrc: 'lrc1.lrc',
+          theme: '#ebd0c2'
+      },
+      {
+          name: 'name2',
+          artist: 'artist2',
+          url: 'url2.mp3',
+          cover: 'cover2.jpg',
+          lrc: 'lrc2.lrc',
+          theme: '#46718b'
+      }
+  ]
+});
