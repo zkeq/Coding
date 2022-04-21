097ef9546939d7b8d8717a4b8a41282cc30bb4ff
Zkeq <learnonly@learnonly.xyz>
2022-04-22 00:15:36 +0800

    测试 站点统计

--- source/js/search-button.js
+++ source/js/search-button.js
@@ -5,3 +5,15 @@ docsearch({
   container: '#search-button',
   debug: false
 });
+
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://icodeq.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='//cdn.matomo.cloud/icodeq.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
