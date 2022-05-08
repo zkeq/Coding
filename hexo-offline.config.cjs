56d08d0bd5fa942c14ca7016c4e11b09825fc2a0
zkeq <admin@icodeq.com>
2022-05-08 20:14:17 +0800

    hexo-offline

---
+++ hexo-offline.config.cjs
@@ -0,0 +1,26 @@
+// offline config passed to workbox-build.
+module.exports = {
+    globPatterns: ["**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}"],
+    globDirectory: "/path/to/hexo/public",
+    swDest: "/path/to/hexo/service-worker.js",
+  },
+  // offline config passed to workbox-build.
+module.exports = {
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/s-gz-2220-icodeq\.oss\.dogecdn\.com\/.*/,
+        handler: "CacheFirst"
+      },
+      {
+        urlPattern: /^https:\/\/npm\.elemecdn\.com\/.*/,
+        handler: "CacheFirst"
+      },
+      {
+        urlPattern: /^https:\/\/cdn\.plyr\.io\/.*/,
+        handler: "CacheFirst"
+      },      {
+        urlPattern: /^https:\/\/ik\.imagekit\.io\/.*/,
+        handler: "CacheFirst"
+      }
+    ]
+  }
