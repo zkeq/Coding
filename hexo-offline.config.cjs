ce04ab0844933c997df891ae62baa072c1ecb9c6
zkeq <admin@icodeq.com>
2022-06-17 20:16:39 +0800

    test del

--- hexo-offline.config.cjs
+++ hexo-offline.config.cjs
@@ -1,7 +1,6 @@
 // offline config passed to workbox-build.
 module.exports = {
     globPatterns: ["**/*.{js,css,png,jpg,gif,svg,eot,ttf,woff}"],
-    globDirectory: "/path/to/hexo/public",
     swDest: "/path/to/hexo/service-worker.js",
   },
   // offline config passed to workbox-build.
