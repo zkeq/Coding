cba584144c003f2ae4e01341b74fb1f1c8f9bc4f
zkeq <admin@icodeq.com>
2022-06-17 20:01:20 +0800

    不缓存 HTML ?

--- hexo-offline.config.cjs
+++ hexo-offline.config.cjs
@@ -1,6 +1,6 @@
 // offline config passed to workbox-build.
 module.exports = {
-    globPatterns: ["**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}"],
+    globPatterns: ["**/*.{js,css,png,jpg,gif,svg,eot,ttf,woff}"],
     globDirectory: "/path/to/hexo/public",
     swDest: "/path/to/hexo/service-worker.js",
   },
