399372edbaa0a9f5742541bab674bd6927176dba
zkeq <admin@icodeq.com>
2022-06-19 11:55:49 +0800

    NetworkOnly

--- hexo-offline.config.cjs
+++ hexo-offline.config.cjs
@@ -1,6 +1,6 @@
 // offline config passed to workbox-build.
 module.exports = {
-    globPatterns: ["**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}"],
+    globPatterns: ["**/*.{js,css,png,jpg,gif,svg,eot,ttf,woff}"],
     globDirectory: "/path/to/hexo/public",
     swDest: "/path/to/hexo/service-worker.js",
   };
@@ -25,7 +25,7 @@ module.exports = {
       },
       {
         urlPattern: /.*\.html/,
-        handler: 'NetworkFirst'
+        handler: 'NetworkOnly'
       },
       {
         urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
