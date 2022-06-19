90f6b177c046f85fd8f5c342c0922ad58df7fd41
zkeq <admin@icodeq.com>
2022-06-19 11:37:01 +0800

    fix workbox-build config

--- hexo-offline.config.cjs
+++ hexo-offline.config.cjs
@@ -30,44 +30,14 @@ module.exports = {
       {
         urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
         handler: 'cacheFirst', // 缓存优先
-        options: {
-          plugins: [
-            {
-              expiration: {
-                maxAgeSeconds: 24 * 60 * 60, // 最长缓存时间,
-                maxEntries: 50 // 最大缓存图片数量
-              }
-            }
-          ]
-        }
       },
       {
         urlPattern: /.*\.css/,
         handler: 'staleWhileRevalidate', // 缓存优先同时后台更新
-        options: {
-          // 这里可以设置 cacheName 和添加插件
-          plugins: [
-            {
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          ]
-        }
       },
       {
         urlPattern: /.*\.js/,
         handler: 'staleWhileRevalidate', // 缓存优先同时后台更新
-        options: {
-          // 这里可以设置 cacheName 和添加插件
-          plugins: [
-            {
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          ]
-        }
       }
     ]
   }
