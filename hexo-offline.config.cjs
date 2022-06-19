e2ab6c58ef4a27b6a1292a2edb4d1b15b50987e3
zkeq <admin@icodeq.com>
2022-06-19 11:34:26 +0800

    update workbox-build config

--- hexo-offline.config.cjs
+++ hexo-offline.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     globPatterns: ["**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}"],
     globDirectory: "/path/to/hexo/public",
     swDest: "/path/to/hexo/service-worker.js",
-  },
+  };
   // offline config passed to workbox-build.
 module.exports = {
     runtimeCaching: [
@@ -22,6 +22,52 @@ module.exports = {
       {
         urlPattern: /^https:\/\/ik\.imagekit\.io\/.*/,
         handler: "CacheFirst"
+      },
+      {
+        urlPattern: /.*\.html/,
+        handler: 'networkFirst'
+      },
+      {
+        urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
+        handler: 'cacheFirst', // 缓存优先
+        options: {
+          plugins: [
+            {
+              expiration: {
+                maxAgeSeconds: 24 * 60 * 60, // 最长缓存时间,
+                maxEntries: 50 // 最大缓存图片数量
+              }
+            }
+          ]
+        }
+      },
+      {
+        urlPattern: /.*\.css/,
+        handler: 'staleWhileRevalidate', // 缓存优先同时后台更新
+        options: {
+          // 这里可以设置 cacheName 和添加插件
+          plugins: [
+            {
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          ]
+        }
+      },
+      {
+        urlPattern: /.*\.js/,
+        handler: 'staleWhileRevalidate', // 缓存优先同时后台更新
+        options: {
+          // 这里可以设置 cacheName 和添加插件
+          plugins: [
+            {
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          ]
+        }
       }
     ]
   }
