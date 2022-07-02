b002ad353f4ed0ffd2ebc78049493f9a49e740d8
zkeq <admin@icodeq.com>
2022-07-02 09:58:08 +0800

    fxi volume

--- source/js/index_music.js
+++ source/js/index_music.js
@@ -3,8 +3,8 @@ function voicePaly () {
     auio = "https://media.onmicrosoft.cn/Tales%20From%20The%20Loop%20S01E03%20Stasis%20%20%5B2160p%20NV%2010bit%20Joy%5D_01.flac";
     auio = new Audio(auio); 
     auio.loop = true;
-    auio.volume = 0.2;
     auio.play();
+    auio.volume = 0.2;
 }
 document.getElementById("body-wrap").addEventListener("click", function(){
     voicePaly();
