73b1b788c65b517fcda2f1c1dd1094bad8f910d1
zkeq <admin@icodeq.com>
2022-07-02 10:48:19 +0800

    fix 流量太大了

--- source/js/index_music.js
+++ source/js/index_music.js
@@ -1,6 +1,6 @@
 var auio;
 function voiceInit () {
-    auio = "https://media.onmicrosoft.cn/Tales%20From%20The%20Loop%20S01E03%20Stasis%20%20%5B2160p%20NV%2010bit%20Joy%5D_01.flac";
+    auio = "https://media.onmicrosoft.cn/backgroud.mp3";
     auio = new Audio(auio); 
     auio.loop = true; 
     auio.volume = 0.2;
