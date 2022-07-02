059dc28fc534a37215b0bf17773ca09410641763
zkeq <admin@icodeq.com>
2022-07-02 10:22:13 +0800

    fix more

--- source/js/index_music.js
+++ source/js/index_music.js
@@ -1,15 +1,18 @@
 var auio;
-function voicePaly () {
+function voiceInit () {
     auio = "https://media.onmicrosoft.cn/Tales%20From%20The%20Loop%20S01E03%20Stasis%20%20%5B2160p%20NV%2010bit%20Joy%5D_01.flac";
     auio = new Audio(auio); 
-    auio.loop = true;
-    auio.play();
+    auio.loop = true; 
     auio.volume = 0.2;
+}
+function voicePaly () {
+    auio.play();
 }
 function voiceStop () {
     auio.pause();
 }
 window.onload = function(){
+voiceInit()
 document.getElementById("body-wrap").addEventListener("click", function(){
     voicePaly();
     });
