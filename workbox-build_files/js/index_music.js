74f318435202b87699a9dbc94c25d504e93ec321
zkeq <admin@icodeq.com>
2022-07-04 17:39:46 +0800

    fix bug

--- workbox-build_files/js/index_music.js
+++ workbox-build_files/js/index_music.js
@@ -40,6 +40,7 @@ setInterval(function () {
         NProgress.inc();
     }
     if (aplayers !== null && initap === 0) {
+        setTimeout(function () {
         let ap = aplayers[0];
         ap.on('play', function () {
             voiceStop();
@@ -47,6 +48,7 @@ setInterval(function () {
         ap.on('pause', function () {
             voicePaly();
         });
+        }, 1000);
         initap ++;
     }
 }, 200)
